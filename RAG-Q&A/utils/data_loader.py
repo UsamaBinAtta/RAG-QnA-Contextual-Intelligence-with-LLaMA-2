d91a6d7a1144c19bd9ad03bc8834dd2abce05987
UsamaBinAtta <usaama.atta@gmail.com>
2024-11-16 22:53:53 +0500

    Delete RAG-Q&A directory

--- RAG-Q&A/utils/data_loader.py
+++
@@ -1,7 +0,0 @@
-from llama_index.core import SimpleDirectoryReader
-
-def load_documents(data_path):
-    reader = SimpleDirectoryReader(data_path)
-    documents = reader.load_data()
-    print("Documents Loaded: ", documents)
-    return documents
--- RAG-Q&A/utils/prompts.py
+++
@@ -1,9 +0,0 @@
-from llama_index.core.prompts.prompts import SimpleInputPrompt
-
-def define_prompts():
-    system_prompt = """
-    You are a Q&A assistant. Your goal is to answer questions accurately based on the provided context.
-    If the question is unrelated, politely indicate that you answer only context-related questions.
-    """
-    query_wrapper_prompt = SimpleInputPrompt("<|USER|>{query_str}<|ASSISTANT|>")
-    return system_prompt, query_wrapper_prompt
